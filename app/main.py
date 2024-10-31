1a7556a75bb03a8208673c14a2336fdae15ddf51
Sam Vandenabeele <sam.vandenabeele.09@gmail.com>
2024-10-31 20:14:23 +0100

    Refactor devcontainer.json, .gitignore, main.py, waveform.py, and DOCKERFILE

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, send_file, jsonify, request, redirect # type: ignore
-from flask_socketio import SocketIO # type: ignore
+from flask import Flask, render_template, send_file, jsonify, request, redirect, session # type: ignore
+from flask_socketio import SocketIO, emit # type: ignore
 import os
 from werkzeug.utils import secure_filename # type: ignore
 from app.scripts import waveform
+import random, string
+from threading import Thread, Lock
+from time import sleep
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'data/videos'
-socket = SocketIO(app)
+socketio = SocketIO(app)
 
-def callback(progress, total):
-    print(progress, total)
+app.secret_key = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
+file_lock = Lock()
 
 @app.route('/')
 def index():
@@ -17,8 +20,11 @@ def index():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    def statusbalk(progress, total):
-        print("Statusbalk progress: ", progress, total)
+    def callback(progress, total):
+        socketio.emit("progress", {"progress": progress, "total": total}, namespace='/')
+        socketio.sleep(0)  # Yield to the event loop to ensure the emit is processed
+
+    session["filename"] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
     if 'files[]' not in request.files:
         return jsonify({"message": "No file part in the request"}), 400
@@ -31,30 +37,26 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], session["filename"] + ".mp3")
             if not os.path.exists(file_path):
                 open(file_path, 'a').close()
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-            print(statusbalk)
-            waveform.generate_waveform_video(file_path, callback, 4, 4)
+            file.save(file_path)
+            waveform.generate_waveform_video(file_path, callback, os.path.join(app.config['UPLOAD_FOLDER'], session['filename'] + ".mp4"), 4, 4)
         else:
             return jsonify({"message": f"File type not allowed: {file.filename}"}), 400
-    return redirect('/download/output.mp4')
+    return redirect("/download/" + session['filename'] + ".mp4")
 
 @app.route('/download/<filename>', methods=['GET'])
 def download(filename):
     file_path = os.path.join("data/videos", filename)
     if os.path.exists(file_path):
-        
         return send_file(os.path.join("../data/videos", filename), as_attachment=True, mimetype="Content-Type: video/mp4; charset=UTF-8")
     else:
         return jsonify({"message": "File not found", "path": file_path}), 404
-    
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'mp3'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
-    socket.run(app, debug=True)
+    socketio.run(app, debug=True)
--- app/scripts/waveform.py
+++ app/scripts/waveform.py
@@ -3,7 +3,6 @@ import cv2 #type: ignore
 from scipy.io import wavfile #type: ignore
 from pydub import AudioSegment #type: ignore
 from moviepy.editor import AudioFileClip, VideoFileClip #type: ignore
-from concurrent.futures import ThreadPoolExecutor
 import os
 from app.scripts.backgrounds import backgrounds as bg
 
@@ -18,32 +17,23 @@ def progressbar(progress, total):
 
 def mp3_to_wav(mp3_filename):
     audio = AudioSegment.from_mp3(mp3_filename)
-    wav_filename = "data/videos/soundscape.wav"
+    wav_filename = mp3_filename.replace('.mp3', '.wav')
     audio.export(wav_filename, format="wav")
     return wav_filename
 
 def generate_waveform_frame(samples, generator, width, height, fps):
-    # print(samples)
+    # frame = next(generator)
     frame = np.zeros((height*2, width*2, 3), dtype=np.uint8)
-    frame = next(generator)
     for x in range(len(samples) - 1):
-        # print(len(samples))
         start_point = (int(x * width * 2 / len(samples)), int((1 - float(samples[x, 0])) * height))
         end_point = (int((x + 1) * width * 2 / len(samples)), int((1 - samples[x + 1, 0]) * height))
         cv2.line(frame, start_point, end_point, (255, 255, 255), 1)
-    frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_LINEAR)
     _, frame = cv2.threshold(frame, 10, 255, cv2.THRESH_BINARY)
     return frame
 
-def frame_generator(wav_filename, sample_rate=44100, frame_rate=12, sub_frame_rate=2):
+def frame_generator(wav_filename, callback, sample_rate=44100, frame_rate=12, sub_frame_rate=2):
     height, width = 480, 640  # Frame size
-    print("height", type(height))
-    print("width", type(width))
-    print("wav_filename", type(wav_filename))
-    print("sample_rate", type(sample_rate))
-    print("frame_rate", type(frame_rate))
-    print("sub_frame_rate", type(sub_frame_rate))
-    bg_gen = bg.background_generator('data/videos/soundscape.wav', fps=frame_rate * (sub_frame_rate + 1), width=width*2, height=height*2)
+    bg_gen = bg.background_generator(wav_filename, fps=frame_rate * (sub_frame_rate + 1), width=width*2, height=height*2)
     sample_rate, samples = wavfile.read(wav_filename)
     samples_per_frame = int(sample_rate / frame_rate)
     samples = samples / np.max(np.abs(samples), axis=0)  # Normalize the samples
@@ -61,10 +51,12 @@ def frame_generator(wav_filename, sample_rate=44100, frame_rate=12, sub_frame_ra
         
         frame = generate_waveform_frame(samples[start_idx:end_idx], bg_gen, width, height, fps=frame_rate*(sub_frame_rate+1))
         frames.append(frame)
+        callback(frame_number+1, total_frames)
 
         for sub_frame_number in range(sub_frame_rate):
             start_idx += subframe_step
             end_idx += subframe_step
+            print(f"start_idx: {start_idx}, end_idx: {end_idx}")
             frame = generate_waveform_frame(samples[start_idx:end_idx], bg_gen, width, height, fps=frame_rate*(sub_frame_rate+1))
             frames.append(frame)
 
@@ -74,64 +66,53 @@ def create_waveform_video(mp3_filename, frame_gen, callback, output_video_filena
     try:
         frames, _, _ = next(frame_gen)
     except StopIteration:
-        print("Error: No frames generated.")
+        print("Error: No frames generated.", flush=True)
         return
 
     height, width, _ = frames[0].shape
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    print(output_video_filename)
-    print(type(output_video_filename))
     out = cv2.VideoWriter(output_video_filename, fourcc, frame_rate*(sub_frame_rate+1), (width, height))
 
     frame_count = 0
     for frame in frames:
         out.write(frame)
-        frame_count += 1
-
+    frame_count += 1
     for frames, frame_number, total_frames in frame_gen:
         for frame in frames:
             out.write(frame)
         frame_count += 1
-        progressbar(frame_number, total_frames)
         callback(frame_number, total_frames)
-
 
     out.release()
 
     if frame_count == 0:
-        print("Error: No frames were written to the video.")
+        print("Error: No frames were written to the video.", flush=True)
         return
 
     audio_clip = AudioFileClip(mp3_filename)
     video_clip = VideoFileClip(output_video_filename)
 
-    print(f"Audio duration: {audio_clip.duration}")
-    print(f"Video duration: {video_clip.duration}")
-
-    # Adjust video duration to match the audio duration
     final_duration = min(audio_clip.duration, video_clip.duration)
-    print(f"Final duration: {final_duration}")
     final_clip = video_clip.set_audio(audio_clip.subclip(0, final_duration))
-    
-    final_clip.write_videofile("data/videos/soundscape.mp4", codec="libx264", fps=frame_rate*(sub_frame_rate+1), audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
     video_clip.close()
-    bg.close()
-    os.remove('data/videos/soundscape.wav')
-    # os.remove('data/videos/output_waveform_temp.mp4')
+
+
+    final_clip.write_videofile(output_video_filename, codec="libx264", fps=frame_rate*(sub_frame_rate+1), audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
+    # audio_clip.close()
+    # bg.close()
+    os.remove(mp3_filename)
 
 def generate_waveform_video(mp3_filename, callback, output_video_filename="data/videos/output.mp4", frame_rate=12, sub_frame_rate=2):
     if not os.path.isfile(mp3_filename):
         print(f"Error: The file '{mp3_filename}' does not exist.")
         return
 
+    print("converting mp3 to wav", flush=True)
     wav_filename = mp3_to_wav(mp3_filename)
-    frames = frame_generator(wav_filename, frame_rate=frame_rate, sub_frame_rate=sub_frame_rate)
-    create_waveform_video(mp3_filename, frames, callback, frame_rate=frame_rate, sub_frame_rate=sub_frame_rate)
+    print("generating generator", flush=True)
+    frames = frame_generator(wav_filename, callback=callback, frame_rate=frame_rate, sub_frame_rate=sub_frame_rate)
+    print("creating video", flush=True)
+    create_waveform_video(mp3_filename, frames, callback, output_video_filename=output_video_filename, frame_rate=frame_rate, sub_frame_rate=sub_frame_rate)
     print(f"Video saved as {output_video_filename}")
 
-if __name__ == "__main__":
-    mp3_filename = input("Enter the path to the MP3 file: ").strip()
-    frame_rate = int(input("Enter the frame rate: ").strip())
-    sub_frame_rate = int(input("Enter the sub-frame rate: ").strip())
-    generate_waveform_video(mp3_filename, frame_rate=frame_rate, sub_frame_rate=sub_frame_rate)
-    
+    return
